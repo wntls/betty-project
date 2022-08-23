35cd78f58c893406dd44328ef77900c5006d0fb7
test1 <jh1675s@naver.com>
2022-08-23 21:30:50 +0900

    [ADD] 8/23 member cont, serv : login 기능

--- src/main/java/com/koreate/betty/domain/member/controller/SignController.java
+++ src/main/java/com/koreate/betty/domain/member/controller/SignController.java
@@ -1,18 +1,32 @@
 package com.koreate.betty.domain.member.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.koreate.betty.domain.member.dto.LoginForm;
+import com.koreate.betty.domain.member.service.MemberService;
+import com.koreate.betty.domain.member.vo.Member;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/sign")
-public class SignController {
+@RequiredArgsConstructor
+public class SignController {	
 	
-	@GetMapping("in")
+	private final MemberService ms;
+	
+	@GetMapping("inin")
 	public String login() {
+		
+		
 		return "sign/sign-in";
 	}
-	
+		
 	@GetMapping("up")
 	public String signUpChoose() {
 		return "sign/sign-up-choose";
@@ -27,4 +41,17 @@ public class SignController {
 	public String signUp_staff() {
 		return "sign/sign-up-staff";
 	}
+	
+
+	@GetMapping("in")
+	public String login(LoginForm form, HttpSession session) {
+		Member loginMember = ms.loginMember(form);
+		session.setAttribute("loginMember", loginMember);
+		return "redirect:/";
+	}
+
+	
+	
+	
+	
 }
