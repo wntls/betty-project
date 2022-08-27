f10a67cfcb044e97153a657f26c1bff6a04b3203
namu6747 <namu6747@naver.com>
2022-08-27 15:08:59 +0900

    +

--- src/main/java/com/koreate/betty/global/controller/TestApiController.java
+++ src/main/java/com/koreate/betty/global/controller/TestApiController.java
@@ -1,6 +1,8 @@
 package com.koreate.betty.global.controller;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,16 +43,6 @@ public class TestApiController {
 		String code;
 		String title;
 		String content;
-	}
-	
-	@PostMapping("/test/multi")
-	@ResponseBody
-	public Data testMulti(@RequestBody Data data) {
-		String code = data.getCode();
-		log.info("code = {}", data.getCode());
-		data.setTitle(code+" title");
-		data.setContent(code+" content");
-		return data;
 	}
 
 }
