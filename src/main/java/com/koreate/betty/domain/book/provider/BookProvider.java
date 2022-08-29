4d30f5ab602b8fa5556b304ca3d4912b754e3e8e
test1 <jh1675s@naver.com>
2022-08-29 17:28:56 +0900

    [UPDATE] Book provider, service

--- src/main/java/com/koreate/betty/domain/book/provider/BookProvider.java
+++ src/main/java/com/koreate/betty/domain/book/provider/BookProvider.java
@@ -1,8 +1,8 @@
 package com.koreate.betty.domain.book.provider;
-import static com.koreate.betty.domain.model.TableConst.RENTAL_TBL;
 import static com.koreate.betty.domain.model.TableConst.BOOK_COMMENT_TBL;
 import static com.koreate.betty.domain.model.TableConst.BOOK_SINGLE_TBL;
 import static com.koreate.betty.domain.model.TableConst.BOOK_TBL;
+import static com.koreate.betty.domain.model.TableConst.RENTAL_TBL;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.jdbc.SQL;
 
+import com.koreate.betty.domain.book.dto.form.BookDeleteForm;
 import com.koreate.betty.domain.book.dto.form.BookSearchForm;
 import com.koreate.betty.domain.book.vo.Book;
 import com.koreate.betty.domain.book.vo.BookComment;
@@ -54,6 +55,13 @@ public class BookProvider {
 		return new SQL().SELECT("*").FROM(BOOK_COMMENT_TBL).WHERE("code = #{code}").OFFSET("#{cri.startRow}")
 				.LIMIT("#{cri.perPageNum}").toString();
 	}
+	
+	// 도서 상세 정보 페이지의 댓글 총계
+	public String findCommentByCodeCount(String code) {
+		return new SQL().SELECT("count(*)").FROM(BOOK_COMMENT_TBL)
+				.WHERE("code = #{code}")
+				.toString();
+	}
 
 	// 신규 도서 등록
 	public String insert(Book book) {
@@ -89,19 +97,28 @@ public class BookProvider {
 	 */
 
 	// 도서 전체 검색 // 테스트 완료
-	public String jFindAll() {
+	public String jFindAll(Criteria cri) {
 		return new SQL().SELECT("*").FROM(BOOK_TBL).JOIN(BOOK_SINGLE_TBL).WHERE("code = book_code").ORDER_BY("code")
-				.ORDER_BY("num").toString();
+				.ORDER_BY("num")
+				.OFFSET("#{startRow}")
+				.LIMIT("#{perPageNum}")
+				.toString();
+	}
+	
+	// 도서 전체 검색 갯수
+	public String jFindAllCount() {
+		return new SQL().SELECT("count(*)").FROM(BOOK_TBL).JOIN(BOOK_SINGLE_TBL).WHERE("code = book_code").ORDER_BY("code")
+				.toString();
 	}
 
-	// code로 한 도서의 목록 검색 (isbn은 동일한 책이 여럿이 나옴)
+	// code로 찾은 한 도서의 목록 (isbn은 동일한 책이 여럿이 나옴) -> 특정 책을 클릭 했을 때, 책의 상태를 확인하기 위함
 	public String jFindByCode(String code) {
 		return new SQL().SELECT("*").FROM(BOOK_TBL).JOIN(BOOK_SINGLE_TBL).WHERE("code = book_code").ORDER_BY("code")
 				.WHERE("code = #{code}").ORDER_BY("num").toString();
 	}
 
 	// 개별 도서 검색(복합 검색) ex) 대여현황
-	public String jSearch(BookSearchForm form, Criteria cri) {
+	public String jSearchCount(BookSearchForm form, Criteria cri) {
 
 		String search = form.getSearchText();
 		String searchOption = form.getSearchOption();
@@ -109,7 +126,7 @@ public class BookProvider {
 		String pubDateOption = form.getPubDateOption();
 		String submitPage = form.getSubmitPage();
 		
-		SQL sql = new SQL().SELECT("*");
+		SQL sql = new SQL().SELECT("count(*)");
 
 		
 		// View 페이지에서 input hidden 으로 name : submitPage 
@@ -178,6 +195,80 @@ public class BookProvider {
 		
 		return sql.toString();
 	}
+	
+	public String jSearchCount(BookSearchForm form) {
+		String search = form.getSearchText();
+		String searchOption = form.getSearchOption();
+		String rentOption = form.getRentOption();
+		String pubDateOption = form.getPubDateOption();
+		String submitPage = form.getSubmitPage();
+		
+		SQL sql = new SQL().SELECT("*");
+
+		
+		// View 페이지에서 input hidden 으로 name : submitPage 
+		// value 값을 books, rentals로 보내야 합니다
+		if (submitPage.equals("books"))	{ // 도서 현황 'staff/books'
+			sql.FROM(BOOK_TBL);
+		} else if (submitPage.equals("rentals")) { // 대여 현황 'staff/rentals'
+			sql.FROM(BOOK_SINGLE_TBL).JOIN(RENTAL_TBL);
+		} else {
+			System.out.println("========================\n\n\n\n");
+			System.out.println("jSearch : submitPage 값이 이상합니다.");
+			System.out.println("========================\n\n\n\n");			
+			return null;	// View 페이지 정상적으로 연결 시 else 삭제 (if문 전체 날리면 안됩니다)
+		}
+
+		switch (searchOption) {
+		case "title":
+			sql.WHERE("title = " + search);
+			break;
+		case "content":
+			sql.WHERE("content = " + search);
+			break;
+		case "auth":
+			sql.WHERE("auth = " + search);
+			break;
+		case "pub":
+			sql.WHERE("pub = " + search);
+			break;
+		default:
+			break;
+		}
+		if (rentOption != null) { // null일 경우 전체 검색이고, 옵션값을 rent 또는 reserve로 해주세요
+			switch (rentOption) {
+			case "able":
+				sql.WHERE("rental = 'n'");
+				break;
+			case "rent":
+				sql.WHERE("rental = 'y'");
+				break;
+			case "reserv":
+				sql.WHERE("rental = 'r'");
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (form.getPubDate() != null) { // 테스트 필요
+			Timestamp pubDate = Timestamp.valueOf(form.getPubDate() + " 00:00:00");
+			String date = new SimpleDateFormat("yyyy-MM-dd").format(pubDate);
+			if (pubDateOption.equals("after")) {
+				sql.WHERE("pub_date >= " + date);
+			} else {
+				sql.WHERE("pub_date <= " + date);
+			}
+		}
+
+		String genre = form.getGenre();
+		if (genre != null && !genre.equals("")) {
+			sql.WHERE("genre = " + genre);
+		}
+		
+		return sql.toString();
+	}
+	
 
 //	둘중 하나로 검색, 대시보드와 내 대여 현황에 사용(Rental 패키지로 이동하는게 좋을듯 함) 
 //	Rental 패키지에 제대로 있으면 지워주세요
--- src/main/java/com/koreate/betty/domain/book/service/BookService.java
+++ src/main/java/com/koreate/betty/domain/book/service/BookService.java
@@ -1,6 +1,8 @@
 package com.koreate.betty.domain.book.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import com.koreate.betty.domain.book.dto.form.BookDeleteForm;
 import com.koreate.betty.domain.book.dto.form.BookSearchForm;
 import com.koreate.betty.domain.book.dto.form.BookUpdateForm;
 import com.koreate.betty.domain.book.vo.Book;
+import com.koreate.betty.domain.book.vo.JBookSingle;
 import com.koreate.betty.global.util.Criteria;
 import com.koreate.betty.global.util.PageMaker;
 
@@ -21,9 +24,10 @@ public class BookService {
 	@Autowired
 	BookRepository bookRepository;
 	
-	// 이거 맵 객체로 반환할 것 (현재 미처리, 처리시 주석 지움)
+	// 페이징 처리가 필요한 뷰는 맵 객체로 반환합니다. 목록 key : list,   PageMaker key : pm
 	// 책 목록 (books) : 매개변수가 null이면 해당 검색 옵션이 없이 출력합니다
-	public List<Book> bookList(String title, Integer genre, Integer page) {		
+	public Map<String, Object> bookList(String title, Integer genre, Integer page) {		
+		Map<String, Object> map = new HashMap<>();
 		
 		Criteria cri = new Criteria();
 		if (page != null) {
@@ -31,8 +35,30 @@ public class BookService {
 		}
 		int totalCount = bookRepository.findAllCount();
 		PageMaker pageMaker = new PageMaker(cri, totalCount);
-		List<Book> list = bookRepository.findAll(title, genre, cri);
-		return list;
+		
+		map.put("list", bookRepository.findAll(title, genre, cri));
+		map.put("pm", pageMaker);
+		return map;
+	}
+	
+	// 책 상세 정보
+	public Book bookDetail(String code) {
+		return bookRepository.bookDetail(code);
+	}
+	
+	// 책 1개 싱글 + 정보
+	public JBookSingle single(String code, Integer num) {
+		return bookRepository.single(code, num);
+	}
+	
+	// 한 도서의 재고 (대여 예약 미포함)	
+	public int countExistByCode(String code) {
+		return bookRepository.countExistByCode(code);
+	}
+
+	// 한 도서의 총 갯수 (대여 예약 포함)	
+	public int countAllByCode(String code) {
+		return bookRepository.countAllByCode(code);
 	}
 	
 	// 도서 정보 등록
@@ -40,11 +66,9 @@ public class BookService {
 		Book book = form.createBook();
 		return bookRepository.insert(book);
 	}
-	
-	
+		
 	// 도서 정보 수정
 	public int update(String targetCode, BookUpdateForm form) {	
-		
 		Book book = form.createBook();		
 		return bookRepository.update(targetCode, book);
 	}
@@ -68,35 +92,42 @@ public class BookService {
 		
 		for (BookDeleteForm del : formList) {
 			result += bookRepository.insertDump(del);
-			result += bookRepository.delete(del.getBookCode());
+			result += bookRepository.delete(del);
 		}
 		return result;
 	}
 	
 	// 도서 정보 + 도서 번호(개별책 전부 출력)
-	public List<Book> jBookList() {
-		return bookRepository.jFindAll();
+	public Map<String, Object> jBookList(Integer page) {
+		Map<String, Object> map = new HashMap<>();
+		Criteria cri = new Criteria();
+		if(page != null) {
+			cri.setPage(page);
+		}
+		int totalCount = bookRepository.jFindAllCount(); 
+		PageMaker pageMaker = new PageMaker(cri, totalCount);
+		map.put("list", bookRepository.jFindAll(cri));
+		map.put("pm", pageMaker);
+		return map;
 	}
 	
 	// 도서 현황 검색 (여기서는 j가 붙었으나 조인하지 않습니다)
-	public List<Book> bookSearch(BookSearchForm form, Integer page) {
-		Criteria cri = new Criteria();
-		if (page != null) {
-			cri.setPage(page);
-		}
-		return bookRepository.jSearch(form, cri);
-	}
-	
 	// 대여 현황 검색 (singleTBL + rentalTBL)
-	public List<Book> rentalSearch(BookSearchForm form, Integer page) {
+	public Map<String, Object> bookSearch(BookSearchForm form, Integer page) {
+		Map<String, Object> map = new HashMap<>();
 		Criteria cri = new Criteria();
 		if (page != null) {
 			cri.setPage(page);
 		}
-		return bookRepository.jSearch(form, cri);
+		
+		int totalCount = bookRepository.jSearchCount(form); 
+		PageMaker pageMaker = new PageMaker(cri, totalCount); 
+		
+		map.put("list", bookRepository.jSearch(form, cri));
+		map.put("pm", pageMaker);
+		return map;
 	}
 	
-
 	public int insertByCrawler(Book book) {
 		return bookRepository.insertByCrawler(book);		
 	}
